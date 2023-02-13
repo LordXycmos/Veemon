d899edc350cb738f49f2dcdfc01a0c3af677013f
Xycmos <ian.kerkenaar@gmail.com>
2023-02-13 12:33:09 +0100

    Stage 1.1
    -	Added some of the textures (characters, crates, weapon, shields).
    -	Changed the main camera’s properties.
    -	Added the platform walls and the HP counter cards.
    -	Created the characters.
    -	Added colliders to the players and walls.
    -	Created player movement scripts.
    -	Created axe prefab.
    -	Created axe destroying script.
    -	Created player throwing script.

---
+++ Veemon/Assets/Scripts/Axe.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+    //Variables
+    public float liveTime = 0.24f;
+    public float rotSpeed;
+
+    void Start()
+    {
+        //Spins the axe
+        transform.Rotate(0, 0, rotSpeed);
+
+        //Destroys the axe after certain amount of time
+        Destroy(this.gameObject, liveTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destroys the axe on collision
+        Destroy(gameObject);
+    }
+}
